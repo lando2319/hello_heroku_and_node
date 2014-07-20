cbd8d5a6bdcd7505c216acb2905a7ad83f1aab9b
Mike Land <mikepland@gmail.com>
2014-07-20 12:27:00 -0500

    adds pg

--- web.js
+++ web.js
@@ -1,6 +1,15 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  client.query('SELECT * FROM your_table', function(err, result) {
+    done();
+    if(err) return console.error(err);
+    console.log(result.rows);
+  });
+});
 
 app.use(logfmt.requestLogger());
 
